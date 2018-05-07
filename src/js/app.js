869406c46f7f691d79ed825d519fbcca29f68c9a
nickjevershed <nick.evershed@theguardian.com>
2018-05-07 16:50:37 +1000

    local file

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import xr from 'xr';
 import { Budgetizer } from './modules/budgetizer'
 
+// process.env.PATH + '/assets/1IKIp4NOuOfOwaduHiutgTvc55joO3DElR3W0k4aAHPU.json'
+
 xr.get('https://interactive.guim.co.uk/docsdata/1IKIp4NOuOfOwaduHiutgTvc55joO3DElR3W0k4aAHPU.json').then((resp) => {
 
 	let googledoc = resp.data.sheets;
